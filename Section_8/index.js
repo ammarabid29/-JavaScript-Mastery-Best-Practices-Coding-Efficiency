46e86dcee383dbb9b6f0e1d649c009ecddb8b54e
ammarabid29 <aammarawan786@gmail.com>
2024-10-10 17:32:18 +0500

    Section 8 Performance Optimization

---
+++ Section_8/index.js
@@ -0,0 +1,27 @@
+
+
+// Code Optimization Techniques 
+
+// 1. Manification
+// Before Manification
+function add(a, b) {
+    return a + b;
+}
+// After Manification
+function add(a, b) { return a + b; }
+
+// 2. Avoid Global Variables
+
+// 3. use for loop
+
+// 4. use DOM: Document Object Model in efficient way
+
+// Leverage Browser Caching
+// 1. Http Caching Headers
+// 2. ETag (Entity Tag)
+// 3. Versioning or Content Hashing
+
+// Optimaztion Strategies
+// 1. Compression
+// 2. Code Splitting
+// 3. Tree Shaking
